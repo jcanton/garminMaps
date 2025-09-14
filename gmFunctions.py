b7ef4010c49ef4e67706b6ccdba98083d69aff8b
Jacopo <jacopo.canton@gmail.com>
2025-09-14 13:16:10 +0200

    some updates after a long time

--- gmFunctions.py
+++ gmFunctions.py
@@ -1,8 +1,7 @@
-#===============================================================================
+# ===============================================================================
 # Parse gpx file
 #
 def gpxParse(fileHandle):
-
     import gpxpy
     import pandas as pd
 
@@ -13,188 +12,211 @@ def gpxParse(fileHandle):
     segment = track.segments[0]
     # Load the data into a Pandas dataframe (by way of a list)
     data = []
-    #segment_length = segment.length_3d()
+    # segment_length = segment.length_3d()
     gpx_df = pd.DataFrame([])
     for point_idx, point in enumerate(segment.points):
-        data.append([point.longitude, point.latitude,point.elevation,
-        point.time, segment.get_speed(point_idx)])
-        columns = ['Longitude', 'Latitude', 'Altitude', 'Time', 'Speed']
+        data.append(
+            [
+                point.longitude,
+                point.latitude,
+                point.elevation,
+                point.time,
+                segment.get_speed(point_idx),
+            ]
+        )
+        columns = ["Longitude", "Latitude", "Altitude", "Time", "Speed"]
         gpx_df = pd.DataFrame(data, columns=columns)
 
     # make points tuple for line
     gpx_points = []
     for track in gpx.tracks:
-        for segment in track.segments: 
+        for segment in track.segments:
             for point in segment.points:
                 gpx_points.append(tuple([point.latitude, point.longitude]))
     return gpx_df, gpx_points, gpx
 
-#===============================================================================
+
+# ===============================================================================
 # Pandas dataframe to GeoJson feature
 # adapted from https://notebook.community/gnestor/jupyter-renderers/notebooks/nteract/pandas-to-geojson
 #
-def df_to_geojsonF(df, properties={}, lat='Latitude', lon='Longitude'):
-
+def df_to_geojsonF(df, properties={}, lat="Latitude", lon="Longitude"):
     # create a feature template to fill in
     feature = {
-            'type':'Feature',
-            'properties': properties,
-            'geometry':{
-                'type':'LineString',
-                'coordinates':[]
-                }
-            }
+        "type": "Feature",
+        "properties": properties,
+        "geometry": {"type": "LineString", "coordinates": []},
+    }
 
     # loop through each row in the dataframe and add its coordinates
     for _, row in df.iterrows():
-        
         # fill in the coordinates
-        feature['geometry']['coordinates'].append( [row[lon],row[lat]] )
+        feature["geometry"]["coordinates"].append([row[lon], row[lat]])
 
     return feature
 
-#===============================================================================
+
+# ===============================================================================
 # Save activities to gpx files
 #
 def activitiesToGpx(dateStart, dateEnd, client, activityTypes, gpxDir, logging):
-
-    import os, sys
+    import os
+    import sys
     from datetime import datetime
+
     from garminconnect import (
+        GarminConnectAuthenticationError,
         GarminConnectConnectionError,
         GarminConnectTooManyRequestsError,
-        GarminConnectAuthenticationError,
     )
 
-    dateFmt   = '%Y-%m-%d'
+    dateFmt = "%Y-%m-%d"
 
-    logging.info('Selected activities: ' + ', '.join(activityTypes))
-    logging.info('')
+    logging.info("Selected activities: " + ", ".join(activityTypes))
+    logging.info("")
     for activityType in activityTypes:
-    
-        logging.info('\tDownloading activities: ' + activityType)
-    
+        logging.info("\tDownloading activities: " + activityType)
+
         outputDir = os.path.join(gpxDir, activityType)
         if not os.path.exists(outputDir):
             os.makedirs(outputDir)
-        
+
         try:
-            activities = client.get_activities_by_date(dateStart.strftime(dateFmt), dateEnd.strftime(dateFmt), activityType)
+            activities = client.get_activities_by_date(
+                dateStart.strftime(dateFmt), dateEnd.strftime(dateFmt), activityType
+            )
         except (
             GarminConnectConnectionError,
             GarminConnectAuthenticationError,
             GarminConnectTooManyRequestsError,
         ) as err:
-            logging.info('\tError occurred during Garmin Connect Client get activities: %s' % err)
-            sys.exit()
+            logging.info(
+                "\tError occurred during Garmin Connect Client get activities: %s" % err
+            )
+            #sys.exit()
         except Exception as err:  # pylint: disable=broad-except
-            logging.info('\tUnknown error occurred during Garmin Connect Client get activities:\n%s' % err)
-            sys.exit()
-        
+            logging.info(
+                "\tUnknown error occurred during Garmin Connect Client get activities:\n%s"
+                % err
+            )
+            #sys.exit()
+
         for activity in activities:
-        
-            activity_id    = activity['activityId']
-            activity_start = datetime.strptime(activity['startTimeLocal'], '%Y-%m-%d %H:%M:%S')
-        
-            output_file = os.path.join(outputDir, '{0:s}.gpx'.format(activity_start.strftime('%Y%m%d_%H%M%S')))
-        
+            activity_id = activity["activityId"]
+            activity_start = datetime.strptime(
+                activity["startTimeLocal"], "%Y-%m-%d %H:%M:%S"
+            )
+
+            output_file = os.path.join(
+                outputDir, "{0:s}.gpx".format(activity_start.strftime("%Y%m%d_%H%M%S"))
+            )
+
             if not os.path.isfile(output_file):
-                gpx_data = client.download_activity(activity_id, dl_fmt=client.ActivityDownloadFormat.GPX)
-                with open(output_file, 'wb') as fb:
-                    fb.write(gpx_data)
-    
-    logging.info('\tDone')
-    logging.info('')
+                try:
+                    gpx_data = client.download_activity(
+                        activity_id, dl_fmt=client.ActivityDownloadFormat.GPX
+                    )
+                    with open(output_file, "wb") as fb:
+                        fb.write(gpx_data)
+                except Exception as err:  # pylint: disable=broad-except
+                    logging.info(
+                        "\tError occurred during Garmin Connect Client download activity:\n%s"
+                        % err
+                    )
+
+    logging.info("\tDone")
+    logging.info("")
     return 0
 
-#===============================================================================
+
+# ===============================================================================
 # Build maps
 #
 def buildMaps(activityTypes, gpxDir, mapDir, logging):
-
     import os
-    import pandas as pd
-    import folium
-    from folium import plugins as fplugins
     from datetime import timedelta
+
+    import folium
+    import pandas as pd
     from dateutil import tz
+    from folium import plugins as fplugins
 
     for activityType in activityTypes:
-    
-        logging.info('\tBuilding map for: ' + activityType)
-    
+        logging.info("\tBuilding map for: " + activityType)
+
         # generate map
         fmap = folium.Map(
-                tiles=None,
-                location=[47.34967, 8.53660],
-                zoom_start=13,
-                control_scale=True,
-                prefer_canvas=True,
-                )
-        folium.TileLayer('Stamen Terrain',      name='Stamen Terrain'     ).add_to(fmap)
-        folium.TileLayer('Stamen Toner',        name='Stamen Toner'       ).add_to(fmap)
-        folium.TileLayer('CartoDB dark_matter', name='CartoDB dark matter').add_to(fmap)
-        folium.TileLayer('OpenStreetMap',       name='OpenStreet Map'     ).add_to(fmap)
+            #tiles=None,
+            tiles="OpenStreetMap",
+            location=[47.34967, 8.53660],
+            zoom_start=13,
+            control_scale=True,
+            prefer_canvas=True,
+        )
+        #folium.TileLayer("Stamen Terrain", name="Stamen Terrain").add_to(fmap)
+        #folium.TileLayer("Stamen Toner", name="Stamen Toner").add_to(fmap)
+        #folium.TileLayer("CartoDB dark_matter", name="CartoDB dark matter").add_to(fmap)
+        #folium.TileLayer("OpenStreetMap", name="OpenStreet Map").add_to(fmap)
         fplugins.Fullscreen(
-                position='topright',
-                title='Fullscreen',
-                title_cancel='Exit Fullscreen',
-                force_separate_button=True,
-                ).add_to(fmap)
-        heatMapData = pd.DataFrame([])
-    
+            position="topright",
+            title="Fullscreen",
+            title_cancel="Exit Fullscreen",
+            force_separate_button=True,
+        ).add_to(fmap)
+        #heatMapData = pd.DataFrame([])
+
         # load gpx files and add tracks to map
         inputDir = os.path.join(gpxDir, activityType)
         gpxFiles = os.listdir(inputDir)
         # create a new python dict to contain our geojson data, using geojson format
-        gjTracks = {'type':'FeatureCollection', 'features':[]}
+        gjTracks = {"type": "FeatureCollection", "features": []}
         for gpxFile in gpxFiles:
             gpx_df, gpx_points, gpx = gpxParse(open(os.path.join(inputDir, gpxFile)))
-            # add data to heatMapData
-            heatMapData = heatMapData.append(gpx_df)
+            ## add data to heatMapData
+            #heatMapData = heatMapData.append(gpx_df)
             #
             # add gpx track to map
-            trackStart = gpx.time.astimezone(tz.tzlocal()).strftime('%Y-%m-%d  %H:%M')
+            trackStart = gpx.time.astimezone(tz.tzlocal()).strftime("%Y-%m-%d  %H:%M")
             geojsonProperties = {
-                    'Time'     : trackStart,
-                    'Distance' : '{0:.2f} km'.format(gpx.length_3d()/1000),
-                    'Duration' : '{0:s}'     .format(str(timedelta(seconds=gpx.get_duration()))),
-                    'Climbed'  : '{0:d} m'   .format(int(gpx.get_uphill_downhill().uphill)),
-                    'Descended': '{0:d} m'   .format(int(gpx.get_uphill_downhill().downhill)),
-                    }
-            feature = df_to_geojsonF(gpx_df, properties=geojsonProperties),
+                "Time": trackStart,
+                "Distance": "{0:.2f} km".format(gpx.length_3d() / 1000),
+                "Duration": "{0:s}".format(str(timedelta(seconds=gpx.get_duration()))),
+                "Climbed": "{0:d} m".format(int(gpx.get_uphill_downhill().uphill)),
+                "Descended": "{0:d} m".format(int(gpx.get_uphill_downhill().downhill)),
+            }
+            feature = (df_to_geojsonF(gpx_df, properties=geojsonProperties),)
             # some bug
             if type(feature) is tuple:
                 feature = feature[0]
             # add this feature (aka, converted dataframe) to the list of features inside our dict
-            gjTracks['features'].append(feature)
-    
-        fplugins.HeatMap(
-                data=heatMapData[['Latitude', 'Longitude']],
-                name='Heat map',
-                radius=12,
-                #max_zoom=13,
-                show=False,
-                ).add_to(fmap)
-    
+            gjTracks["features"].append(feature)
+
+        #fplugins.HeatMap(
+        #    data=heatMapData[["Latitude", "Longitude"]],
+        #    name="Heat map",
+        #    radius=12,
+        #    # max_zoom=13,
+        #    show=False,
+        #).add_to(fmap)
+
         folium.GeoJson(
-                gjTracks,
-                tooltip=folium.GeoJsonTooltip(fields=['Time'], labels=False),
-                popup=folium.GeoJsonPopup(fields=list(geojsonProperties.keys())),
-                style_function     = lambda x: {'color':'blue', 'weight':3, 'opacity':.5},
-                highlight_function = lambda x: {'color':'red',  'weight':3, 'opacity':1},
-                name='GPX tracks'
-                ).add_to(fmap)
-    
+            gjTracks,
+            tooltip=folium.GeoJsonTooltip(fields=["Time"], labels=False),
+            popup=folium.GeoJsonPopup(fields=list(geojsonProperties.keys())),
+            style_function=lambda x: {"color": "blue", "weight": 3, "opacity": 0.5},
+            highlight_function=lambda x: {"color": "red", "weight": 3, "opacity": 1},
+            name="GPX tracks",
+        ).add_to(fmap)
+
         fmap.add_child(folium.LayerControl())
-    
+
         # save map
         outputDir = mapDir
         if not os.path.exists(outputDir):
             os.makedirs(outputDir)
-    
-        fmap.save(os.path.join(mapDir, 'map_{0:s}.html'.format(activityType)))
-    
-    logging.info('\tDone')
-    logging.info('')
+
+        fmap.save(os.path.join(mapDir, "map_{0:s}.html".format(activityType)))
+
+    logging.info("\tDone")
+    logging.info("")
     return 0
--- main.py
+++ main.py
@@ -1,100 +1,346 @@
 #!/usr/bin/env python3
 
 import configparser
-from datetime import date, datetime
-import os, socket, sys
 import logging
-import gmFunctions as gmf
+import os
+import socket
+import sys
+from datetime import date, datetime
+from getpass import getpass
+from pathlib import Path
+
+import requests
 from garminconnect import (
     Garmin,
+    GarminConnectAuthenticationError,
     GarminConnectConnectionError,
     GarminConnectTooManyRequestsError,
-    GarminConnectAuthenticationError,
 )
+from garth.exc import GarthException, GarthHTTPError
 
-#===============================================================================
+import gmFunctions as gmf
+
+# ===============================================================================
 # Data and config
 #
 config = configparser.ConfigParser()
-config.read(os.path.join(os.path.expanduser('~'), '.python-github.cfg'))
-GARMIN_ID = config.get('garmin.maps', 'GARMIN_ID')
-GARMIN_PW = config.get('garmin.maps', 'GARMIN_PW')
-GARMIN_AC = config.get('garmin.maps', 'GARMIN_ACTIVITIES')
-GARMIN_DS = config.get('garmin.maps', 'GARMIN_DATESTART')
+config.read(os.path.join(os.path.expanduser("~"), ".python-github.cfg"))
+GARMIN_ID = config.get("garmin.maps", "GARMIN_ID")
+GARMIN_PW = config.get("garmin.maps", "GARMIN_PW")
+GARMIN_AC = config.get("garmin.maps", "GARMIN_ACTIVITIES")
+GARMIN_DS = config.get("garmin.maps", "GARMIN_DATESTART")
 
-activityTypes = GARMIN_AC.strip().replace(' ', '').split(',')
-cpDateStart     = [int(ymd) for ymd in GARMIN_DS.strip().split('-')]
+activityTypes = GARMIN_AC.strip().replace(" ", "").split(",")
+cpDateStart = [int(ymd) for ymd in GARMIN_DS.strip().split("-")]
 
-today     = date.today()
+today = date.today()
 dateStart = date(cpDateStart[0], cpDateStart[1], cpDateStart[2])
-dateEnd   = today
+dateEnd = today
 
-execLog = 'log_exec.log'
-msgLog  = 'log_msg.log'
-gpxDir = 'gpxFiles'
-mapDir = 'maps'
+execLog = "log_exec.log"
+msgLog = "log_msg.log"
+gpxDir = "gpxFiles"
+mapDir = "maps"
 
-#===============================================================================
+# ===============================================================================
 # Check whether the script already ran today
 #
 if os.path.isfile(execLog):
+    with open(execLog, "r") as filehandle:
+        lastRun = datetime.fromisoformat(filehandle.readline())
 
-    with open(execLog, 'r') as filehandle:
-        lastRun = datetime.fromisoformat( filehandle.readline() )
-    
         if lastRun.date() == today:
             # exit
-            print('Already run today')
+            print("Already run today")
             sys.exit()
 
-#===============================================================================
+# ===============================================================================
 # Initialize logger
 #
-logging.basicConfig(filename = msgLog,
-                    level = logging.INFO,
-                    format = '%(message)s',
-                    filemode = 'w')
+logging.basicConfig(
+    filename=msgLog, level=logging.INFO, format="%(message)s", filemode="w"
+)
 
-logging.info('============= start mapping =============')
-logging.info('Host: ' + socket.gethostname())
-logging.info('Date: ' + datetime.now().strftime('%Y-%m-%d %H:%M'))
+logging.info("============= start mapping =============")
+logging.info("Host: " + socket.gethostname())
+logging.info("Date: " + datetime.now().strftime("%Y-%m-%d %H:%M"))
 
-#===============================================================================
+# ===============================================================================
 # Init Garmin client and log in
 #
+
+
+def safe_api_call(api_method, *args, **kwargs):
+    """
+    Safe API call wrapper with comprehensive error handling.
+
+    This demonstrates the error handling patterns used throughout the library.
+    Returns (success: bool, result: Any, error_message: str)
+    """
+    try:
+        result = api_method(*args, **kwargs)
+        return True, result, None
+
+    except GarthHTTPError as e:
+        # Handle specific HTTP errors gracefully
+        error_str = str(e)
+        status_code = getattr(getattr(e, "response", None), "status_code", None)
+
+        if status_code == 400 or "400" in error_str:
+            return (
+                False,
+                None,
+                "Endpoint not available (400 Bad Request) - Feature may not be enabled for your account",
+            )
+        elif status_code == 401 or "401" in error_str:
+            return (
+                False,
+                None,
+                "Authentication required (401 Unauthorized) - Please re-authenticate",
+            )
+        elif status_code == 403 or "403" in error_str:
+            return (
+                False,
+                None,
+                "Access denied (403 Forbidden) - Account may not have permission",
+            )
+        elif status_code == 404 or "404" in error_str:
+            return (
+                False,
+                None,
+                "Endpoint not found (404) - Feature may have been moved or removed",
+            )
+        elif status_code == 429 or "429" in error_str:
+            return (
+                False,
+                None,
+                "Rate limit exceeded (429) - Please wait before making more requests",
+            )
+        elif status_code == 500 or "500" in error_str:
+            return (
+                False,
+                None,
+                "Server error (500) - Garmin's servers are experiencing issues",
+            )
+        elif status_code == 503 or "503" in error_str:
+            return (
+                False,
+                None,
+                "Service unavailable (503) - Garmin's servers are temporarily unavailable",
+            )
+        else:
+            return False, None, f"HTTP error: {e}"
+
+    except FileNotFoundError:
+        return (
+            False,
+            None,
+            "No valid tokens found. Please login with your email/password to create new tokens.",
+        )
+
+    except GarminConnectAuthenticationError as e:
+        return False, None, f"Authentication issue: {e}"
+
+    except GarminConnectConnectionError as e:
+        return False, None, f"Connection issue: {e}"
+
+    except GarminConnectTooManyRequestsError as e:
+        return False, None, f"Rate limit exceeded: {e}"
+
+    except Exception as e:
+        return False, None, f"Unexpected error: {e}"
+
+
+def get_credentials():
+    """Get email and password from environment or user input."""
+    # email = os.getenv("EMAIL")
+    # password = os.getenv("PASSWORD")
+
+    email = config.get("garmin.maps", "GARMIN_ID")
+    password = config.get("garmin.maps", "GARMIN_PW")
+
+    if not email:
+        email = input("Login email: ")
+    if not password:
+        password = getpass("Enter password: ")
+
+    return email, password
+
+
+def init_api() -> Garmin | None:
+    """Initialize Garmin API with authentication and token management."""
+
+    # Configure token storage
+    tokenstore = os.getenv("GARMINTOKENS", "~/.garminconnect")
+    tokenstore_path = Path(tokenstore).expanduser()
+
+    print(f"🔐 Token storage: {tokenstore_path}")
+
+    # Check if token files exist
+    if tokenstore_path.exists():
+        print("📄 Found existing token directory")
+        token_files = list(tokenstore_path.glob("*.json"))
+        if token_files:
+            print(
+                f"🔑 Found {len(token_files)} token file(s): {[f.name for f in token_files]}"
+            )
+        else:
+            print("⚠️ Token directory exists but no token files found")
+    else:
+        print("📭 No existing token directory found")
+
+    # First try to login with stored tokens
+    try:
+        print("🔄 Attempting to use saved authentication tokens...")
+        garmin = Garmin()
+        garmin.login(str(tokenstore_path))
+        print("✅ Successfully logged in using saved tokens!")
+        return garmin
+
+    except (
+        FileNotFoundError,
+        GarthHTTPError,
+        GarminConnectAuthenticationError,
+        GarminConnectConnectionError,
+    ):
+        print("🔑 No valid tokens found. Requesting fresh login credentials.")
+
+    # Loop for credential entry with retry on auth failure
+    while True:
+        try:
+            # Get credentials
+            email, password = get_credentials()
+
+            print("� Logging in with credentials...")
+            garmin = Garmin(
+                email=email, password=password, is_cn=False, return_on_mfa=True
+            )
+            result1, result2 = garmin.login()
+
+            if result1 == "needs_mfa":
+                print("🔐 Multi-factor authentication required")
+
+                mfa_code = input("Please enter your MFA code: ")
+                print("🔄 Submitting MFA code...")
+
+                try:
+                    garmin.resume_login(result2, mfa_code)
+                    print("✅ MFA authentication successful!")
+
+                except GarthHTTPError as garth_error:
+                    # Handle specific HTTP errors from MFA
+                    error_str = str(garth_error)
+                    if "429" in error_str and "Too Many Requests" in error_str:
+                        print("❌ Too many MFA attempts")
+                        print("💡 Please wait 30 minutes before trying again")
+                        sys.exit(1)
+                    elif "401" in error_str or "403" in error_str:
+                        print("❌ Invalid MFA code")
+                        print("💡 Please verify your MFA code and try again")
+                        continue
+                    else:
+                        # Other HTTP errors - don't retry
+                        print(f"❌ MFA authentication failed: {garth_error}")
+                        sys.exit(1)
+
+                except GarthException as garth_error:
+                    print(f"❌ MFA authentication failed: {garth_error}")
+                    print("💡 Please verify your MFA code and try again")
+                    continue
+
+            # Save tokens for future use
+            garmin.garth.dump(str(tokenstore_path))
+            print(f"💾 Authentication tokens saved to: {tokenstore_path}")
+            print("✅ Login successful!")
+            return garmin
+
+        except GarminConnectAuthenticationError:
+            print("❌ Authentication failed:")
+            print("💡 Please check your username and password and try again")
+            # Continue the loop to retry
+            continue
+
+        except (
+            FileNotFoundError,
+            GarthHTTPError,
+            GarminConnectConnectionError,
+            requests.exceptions.HTTPError,
+        ) as err:
+            print(f"❌ Connection error: {err}")
+            print("💡 Please check your internet connection and try again")
+            return None
+
+        except KeyboardInterrupt:
+            print("\n👋 Cancelled by user")
+            return None
+
+
+def display_user_info(api: Garmin):
+    """Display basic user information with proper error handling."""
+    print("\n" + "=" * 60)
+    print("👤 User Information")
+    print("=" * 60)
+
+    # Get user's full name
+    success, full_name, error_msg = safe_api_call(api.get_full_name)
+    if success:
+        print(f"📝 Name: {full_name}")
+    else:
+        print(f"📝 Name: ⚠️ {error_msg}")
+
+    # Get user profile number from device info
+    success, device_info, error_msg = safe_api_call(api.get_device_last_used)
+    if success and device_info and device_info.get("userProfileNumber"):
+        user_profile_number = device_info.get("userProfileNumber")
+        print(f"🆔 Profile Number: {user_profile_number}")
+    else:
+        if not success:
+            print(f"🆔 Profile Number: ⚠️ {error_msg}")
+        else:
+            print("🆔 Profile Number: Not available")
+
+
 try:
-    client = Garmin(GARMIN_ID, GARMIN_PW)
-    client.login()
+    # Initialize API with authentication (will only prompt for credentials if needed)
+    api = init_api()
+
+    if not api:
+        print("❌ Failed to initialize API. Exiting.")
+        sys.exit(1)
+
+    # Display user information
+    display_user_info(api)
+
 except (
     GarminConnectConnectionError,
     GarminConnectAuthenticationError,
     GarminConnectTooManyRequestsError,
 ) as err:
-    logging.info('Error occurred during Garmin Connect Client login: %s' % err)
+    logging.info("Error occurred during Garmin Connect Client login: %s" % err)
     sys.exit()
 except Exception:  # pylint: disable=broad-except
-    logging.info('Unknown error occurred during Garmin Connect Client login')
+    logging.info("Unknown error occurred during Garmin Connect Client login")
     sys.exit()
-fullName   = client.get_full_name()
-unitSystem = client.get_unit_system()
+fullName = api.get_full_name()
+unitSystem = api.get_unit_system()
 
-logging.info('Garmin user: ' + fullName)
-logging.info('Unit system: ' + unitSystem)
+logging.info("Garmin user: " + fullName)
+logging.info("Unit system: " + unitSystem)
 
-#===============================================================================
+# ===============================================================================
 # Save activities to gpx files
 #
-doneAct = gmf.activitiesToGpx(dateStart, dateEnd, client, activityTypes, gpxDir, logging)
+#doneAct = gmf.activitiesToGpx(dateStart, dateEnd, api, activityTypes, gpxDir, logging)
 
-#===============================================================================
+# ===============================================================================
 # Build maps
 #
 doneMap = gmf.buildMaps(activityTypes, gpxDir, mapDir, logging)
 
-#===============================================================================
+# ===============================================================================
 # Update last exec file
 #
 if (doneAct == 0) and (doneMap == 0):
     # the script ran succesfully
-    with open(execLog, 'w') as filehandle:
+    with open(execLog, "w") as filehandle:
         filehandle.write(datetime.now().isoformat())
